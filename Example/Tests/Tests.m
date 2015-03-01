1af5db519ebe74857eab5f62fb850280ee35a910
Sebastian Waisbrot <seppo0010@gmail.com>
2015-02-28 22:18:56 -0800

    Add tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -6,37 +6,68 @@
 //  Copyright (c) 2014 Sebastian Waisbrot. All rights reserved.
 //
 
-SpecBegin(InitialSpecs)
+#import <objc-rlite/ObjCHirlite.h>
 
-describe(@"these will fail", ^{
+SpecBegin(Hirlite)
 
-    it(@"can do maths", ^{
-        expect(1).to.equal(2);
+static NSString* key = @"key";
+static NSString* value = @"\xe2\x98\x83";
+static ObjCHirlite* rlite;
+describe(@"these will pass", ^{
+    beforeEach(^{
+        rlite = [[ObjCHirlite alloc] init];
     });
 
-    it(@"can read", ^{
-        expect(@"number").to.equal(@"string");
+    it(@"supports null", ^{
+        id reply = [rlite command:@[@"get", key]];
+        XCTAssertEqualObjects([NSNull null], reply);
+    });
+
+    it(@"supports ok", ^{
+        id reply = [rlite command:@[@"set", key, value]];
+        XCTAssertEqualObjects([NSNumber numberWithBool:TRUE], reply);
     });
     
-    it(@"will wait and fail", ^AsyncBlock {
-        
+    it(@"supports unicode string", ^{
+        [rlite command:@[@"set", key, value]];
+        id reply = [rlite command:@[@"get", key]];
+        XCTAssertEqualObjects(value, reply);
+    });
+    
+    it(@"supports integer", ^{
+        id reply = [rlite command:@[@"lpush", key, value, value]];
+        XCTAssertEqualObjects([NSNumber numberWithInt:2], reply);
+    });
+
+    it(@"supports error", ^{
+        id reply = [rlite command:@[@"set", key]];
+        XCTAssertEqualObjects([reply class], [NSException class]);
+    });
+    
+    it(@"supports array", ^{
+        [rlite command:@[@"rpush", key, @"1", @"2", @"3"]];
+        id reply = [rlite command:@[@"lrange", key, @0, @-1]];
+        id expected = @[@"1", @"2", @"3"];
+        XCTAssertEqualObjects(expected, reply);
     });
 });
 
 describe(@"these will pass", ^{
-    
-    it(@"can do maths", ^{
-        expect(1).beLessThan(23);
+    beforeEach(^{
+        NSArray* paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString* documentsDirectory = [paths objectAtIndex:0]; // Get documents folder
+        NSString* path = [documentsDirectory stringByAppendingPathComponent:@"mydb.rld"];
+
+        rlite = [[ObjCHirlite alloc] initWithPath:path];
     });
     
-    it(@"can read", ^{
-        expect(@"team").toNot.contain(@"I");
+    it(@"creates persistent data", ^{
+        [rlite command:@[@"set", key, value]];
     });
-    
-    it(@"will wait and succeed", ^AsyncBlock {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
-            done();
-        });
+
+    it(@"reads persistent data", ^{
+        id reply = [rlite command:@[@"get", key]];
+        XCTAssertEqualObjects(value, reply);
     });
 });
 
