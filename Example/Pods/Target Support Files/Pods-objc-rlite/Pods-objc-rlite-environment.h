85ef6733d05e129d098ad6a0a6cbd6b6b2d17ccb
Sebastian Waisbrot <seppo0010@gmail.com>
2015-02-28 22:06:54 -0800

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-objc-rlite/Pods-objc-rlite-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// objc-rlite
+#define COCOAPODS_POD_AVAILABLE_objc_rlite
+#define COCOAPODS_VERSION_MAJOR_objc_rlite 0
+#define COCOAPODS_VERSION_MINOR_objc_rlite 1
+#define COCOAPODS_VERSION_PATCH_objc_rlite 0
+
